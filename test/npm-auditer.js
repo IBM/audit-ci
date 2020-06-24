c2bad7d3bbdff0f756f4e92923a658f6d353579c
Quinn Turner <quinnturnertech@gmail.com>
2020-06-24 11:14:04 -0400

    feat(allowlist): Change default to allowlist (#154)
    * feat(allowlist): Change default to allowlist
    
    This PR does quite a few things:
    
    - Supports new argument `allowlist`
    merges: advisories, whitelist, and path-whitelist into one argument
    - Deprecates advisories, whitelist, and path-whitelist
    - Warns the user to update to allowlist
    - Adds new argument `show-found`
    to allow config for whether to show when found a vuln
    - Change low vuln test to have a path
    - Adds more warnings to consider not allowlisting
    - Adds additional tests
    
    Technically, this is not a breaking change.
    This is because we gracefully handle the `-a` flag.
    The implementation now has a superset of functionality.
    
    Closes https://github.com/IBM/audit-ci/pull/153
    Closes https://github.com/IBM/audit-ci/issues/151
    Closes https://github.com/IBM/audit-ci/issues/150

--- test/npm-auditer.js
+++ test/npm-auditer.js
@@ -6,6 +6,8 @@
 const { expect } = require("chai");
 const path = require("path");
 const audit = require("../lib/audit").bind(null, "npm");
+const Allowlist = require("../lib/allowlist");
+const { summaryWithDefault } = require("./common");
 
 function config(additions) {
   const defaultConfig = {
@@ -16,8 +18,7 @@ function config(additions) {
       critical: false,
     },
     "report-type": "important",
-    advisories: [],
-    whitelist: [],
+    allowlist: new Allowlist(),
     "show-not-found": false,
     "retry-count": 5,
     directory: "./",
@@ -44,14 +45,12 @@ describe("npm-auditer", function testNpmAuditer() {
       }),
       (_summary) => _summary
     );
-    expect(summary).to.eql({
-      whitelistedModulesFound: [],
-      whitelistedAdvisoriesFound: [],
-      whitelistedAdvisoriesNotFound: [],
-      whitelistedPathsFound: [],
-      failedLevelsFound: ["critical"],
-      advisoriesFound: [663],
-    });
+    expect(summary).to.eql(
+      summaryWithDefault({
+        failedLevelsFound: ["critical"],
+        advisoriesFound: [663],
+      })
+    );
   });
   it("does not report critical severity if it set to false", async () => {
     const summary = await audit(
@@ -61,14 +60,7 @@ describe("npm-auditer", function testNpmAuditer() {
       }),
       (_summary) => _summary
     );
-    expect(summary).to.eql({
-      whitelistedModulesFound: [],
-      whitelistedAdvisoriesFound: [],
-      whitelistedAdvisoriesNotFound: [],
-      whitelistedPathsFound: [],
-      failedLevelsFound: [],
-      advisoriesFound: [],
-    });
+    expect(summary).to.eql(summaryWithDefault());
   });
   it("reports summary with high severity", async () => {
     const summary = await audit(
@@ -79,14 +71,12 @@ describe("npm-auditer", function testNpmAuditer() {
       }),
       (_summary) => _summary
     );
-    expect(summary).to.eql({
-      whitelistedModulesFound: [],
-      whitelistedAdvisoriesFound: [],
-      whitelistedAdvisoriesNotFound: [],
-      whitelistedPathsFound: [],
-      failedLevelsFound: ["high"],
-      advisoriesFound: [690],
-    });
+    expect(summary).to.eql(
+      summaryWithDefault({
+        failedLevelsFound: ["high"],
+        advisoriesFound: [690],
+      })
+    );
   });
   it("reports important info with moderate severity", async () => {
     const summary = await audit(
@@ -97,14 +87,12 @@ describe("npm-auditer", function testNpmAuditer() {
       }),
       (_summary) => _summary
     );
-    expect(summary).to.eql({
-      whitelistedModulesFound: [],
-      whitelistedAdvisoriesFound: [],
-      whitelistedAdvisoriesNotFound: [],
-      whitelistedPathsFound: [],
-      failedLevelsFound: ["moderate"],
-      advisoriesFound: [658],
-    });
+    expect(summary).to.eql(
+      summaryWithDefault({
+        failedLevelsFound: ["moderate"],
+        advisoriesFound: [658],
+      })
+    );
   });
   it("does not report moderate severity if it set to false", async () => {
     const summary = await audit(
@@ -114,86 +102,139 @@ describe("npm-auditer", function testNpmAuditer() {
       }),
       (_summary) => _summary
     );
-    expect(summary).to.eql({
-      whitelistedModulesFound: [],
-      whitelistedAdvisoriesFound: [],
-      whitelistedAdvisoriesNotFound: [],
-      whitelistedPathsFound: [],
-      failedLevelsFound: [],
-      advisoriesFound: [],
-    });
+    expect(summary).to.eql(summaryWithDefault());
   });
-  it("ignores an advisory if it is whitelisted", async () => {
+  it("[DEPRECATED - advisories] ignores an advisory if it is whitelisted", async () => {
     const summary = await audit(
       config({
         directory: testDir("npm-moderate"),
         levels: { moderate: true },
-        advisories: [658],
+        allowlist: Allowlist.mapConfigToAllowlist({ advisories: [658] }),
       }),
       (_summary) => _summary
     );
-    expect(summary).to.eql({
-      whitelistedModulesFound: [],
-      whitelistedAdvisoriesFound: [658],
-      whitelistedAdvisoriesNotFound: [],
-      whitelistedPathsFound: [],
-      failedLevelsFound: [],
-      advisoriesFound: [],
-    });
+    expect(summary).to.eql(
+      summaryWithDefault({
+        allowlistedAdvisoriesFound: [658],
+      })
+    );
   });
-  it("does not ignore an advisory that is not whitelisted", async () => {
+  it("ignores an advisory if it is allowlisted", async () => {
     const summary = await audit(
       config({
         directory: testDir("npm-moderate"),
         levels: { moderate: true },
-        advisories: [659],
+        allowlist: new Allowlist([658]),
       }),
       (_summary) => _summary
     );
-    expect(summary).to.eql({
-      whitelistedModulesFound: [],
-      whitelistedAdvisoriesFound: [],
-      whitelistedAdvisoriesNotFound: [659],
-      whitelistedPathsFound: [],
-      failedLevelsFound: ["moderate"],
-      advisoriesFound: [658],
-    });
+    expect(summary).to.eql(
+      summaryWithDefault({
+        allowlistedAdvisoriesFound: [658],
+      })
+    );
   });
-  it("reports only vulnerabilities with a not whitelisted path", async () => {
+  it("[DEPRECATED - advisories] does not ignore an advisory that is not whitelisted", async () => {
     const summary = await audit(
       config({
-        directory: testDir("npm-whitelisted-path"),
+        directory: testDir("npm-moderate"),
         levels: { moderate: true },
-        "path-whitelist": ["880|github-build>axios"],
+        allowlist: Allowlist.mapConfigToAllowlist({ advisories: [659] }),
       }),
       (_summary) => _summary
     );
-    expect(summary).to.eql({
-      whitelistedModulesFound: [],
-      whitelistedAdvisoriesFound: [],
-      whitelistedAdvisoriesNotFound: [],
-      whitelistedPathsFound: ["880|github-build>axios"],
-      failedLevelsFound: ["moderate"],
-      advisoriesFound: [880],
-    });
+    expect(summary).to.eql(
+      summaryWithDefault({
+        allowlistedAdvisoriesNotFound: [659],
+        failedLevelsFound: ["moderate"],
+        advisoriesFound: [658],
+      })
+    );
   });
-  it("whitelist all vulnerabilities with a whitelisted path", async () => {
+  it("does not ignore an advisory that is not allowlisted", async () => {
     const summary = await audit(
       config({
-        directory: testDir("npm-whitelisted-path"),
+        directory: testDir("npm-moderate"),
         levels: { moderate: true },
-        "path-whitelist": ["880|axios", "880|github-build>axios"],
+        allowlist: new Allowlist([659]),
       }),
       (_summary) => _summary
     );
-    expect(summary).to.eql({
-      whitelistedModulesFound: [],
-      whitelistedAdvisoriesFound: [],
-      whitelistedAdvisoriesNotFound: [],
-      whitelistedPathsFound: ["880|axios", "880|github-build>axios"],
-      failedLevelsFound: [],
-      advisoriesFound: [],
-    });
+    expect(summary).to.eql(
+      summaryWithDefault({
+        allowlistedAdvisoriesNotFound: [659],
+        failedLevelsFound: ["moderate"],
+        advisoriesFound: [658],
+      })
+    );
+  });
+  it("[DEPRECATED - path-whitelist] reports only vulnerabilities with a not whitelisted path", async () => {
+    const summary = await audit(
+      config({
+        directory: testDir("npm-allowlisted-path"),
+        levels: { moderate: true },
+        allowlist: Allowlist.mapConfigToAllowlist({
+          "path-whitelist": ["880|github-build>axios"],
+        }),
+      }),
+      (_summary) => _summary
+    );
+    expect(summary).to.eql(
+      summaryWithDefault({
+        allowlistedPathsFound: ["880|github-build>axios"],
+        failedLevelsFound: ["moderate"],
+        advisoriesFound: [880],
+      })
+    );
+  });
+  it("reports only vulnerabilities with a not allowlisted path", async () => {
+    const summary = await audit(
+      config({
+        directory: testDir("npm-allowlisted-path"),
+        levels: { moderate: true },
+        allowlist: new Allowlist(["880|github-build>axios"]),
+      }),
+      (_summary) => _summary
+    );
+    expect(summary).to.eql(
+      summaryWithDefault({
+        allowlistedPathsFound: ["880|github-build>axios"],
+        failedLevelsFound: ["moderate"],
+        advisoriesFound: [880],
+      })
+    );
+  });
+  it("[DEPRECATED - path-whitelist] whitelist all vulnerabilities with a whitelisted path", async () => {
+    const summary = await audit(
+      config({
+        directory: testDir("npm-allowlisted-path"),
+        levels: { moderate: true },
+        allowlist: Allowlist.mapConfigToAllowlist({
+          "path-whitelist": ["880|axios", "880|github-build>axios"],
+        }),
+      }),
+      (_summary) => _summary
+    );
+    expect(summary).to.eql(
+      summaryWithDefault({
+        allowlistedPathsFound: ["880|axios", "880|github-build>axios"],
+      })
+    );
+  });
+  it("allowlist all vulnerabilities with a allowlisted path", async () => {
+    const summary = await audit(
+      config({
+        directory: testDir("npm-allowlisted-path"),
+        levels: { moderate: true },
+        allowlist: new Allowlist(["880|axios", "880|github-build>axios"]),
+      }),
+      (_summary) => _summary
+    );
+    expect(summary).to.eql(
+      summaryWithDefault({
+        allowlistedPathsFound: ["880|axios", "880|github-build>axios"],
+      })
+    );
   });
   it("reports low severity", async () => {
     const summary = await audit(
@@ -203,14 +244,12 @@ describe("npm-auditer", function testNpmAuditer() {
       }),
       (_summary) => _summary
     );
-    expect(summary).to.eql({
-      whitelistedModulesFound: [],
-      whitelistedAdvisoriesFound: [],
-      whitelistedAdvisoriesNotFound: [],
-      whitelistedPathsFound: [],
-      failedLevelsFound: ["low"],
-      advisoriesFound: [722],
-    });
+    expect(summary).to.eql(
+      summaryWithDefault({
+        failedLevelsFound: ["low"],
+        advisoriesFound: [786],
+      })
+    );
   });
   it("passes with no vulnerabilities", async () => {
     const summary = await audit(
@@ -220,14 +259,7 @@ describe("npm-auditer", function testNpmAuditer() {
       }),
       (_summary) => _summary
     );
-    expect(summary).to.eql({
-      whitelistedModulesFound: [],
-      whitelistedAdvisoriesFound: [],
-      whitelistedAdvisoriesNotFound: [],
-      whitelistedPathsFound: [],
-      failedLevelsFound: [],
-      advisoriesFound: [],
-    });
+    expect(summary).to.eql(summaryWithDefault());
   });
   it("fails with error code ENOTFOUND on a non-existent site", (done) => {
     audit(
