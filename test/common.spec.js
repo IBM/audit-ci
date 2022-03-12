e3253537015912072dab47d13942588d8c3d1bdb
Lutz Lengemann <lutz@lengemann.net>
2022-03-12 09:22:09 +0100

    Move GH-adivisory URL splitting into common

--- test/common.spec.js
+++ test/common.spec.js
@@ -1,5 +1,5 @@
 const { expect } = require("chai");
-const { matchString } = require("../lib/common");
+const { matchString, gitHubAdvisoryUrlToAdvisoryId } = require("../lib/common");
 
 describe("matchString", () => {
   it("works for various prefixes and suffixes of wildcards", () => {
@@ -7,5 +7,12 @@ describe("matchString", () => {
     expect(matchString("12|axios>*", "12|axios>124")).to.eql(true);
     expect(matchString("*|axios>123", "12|axios>123")).to.eql(true);
     expect(matchString("*|axios>*", "12|axios>123")).to.eql(true);
+  });
+});
+
+describe("gitHubAdvisoryUrlToAdvisoryId", () => {
+  it("converts github-advisory-url to just the advisory-id", () => {
+    expect(gitHubAdvisoryUrlToAdvisoryId("https://github.com/advisories/GHSA-qrpm-p2h7-hrv2")).to.eql("GHSA-qrpm-p2h7-hrv2");
+    expect(gitHubAdvisoryUrlToAdvisoryId("https://github.com/advisories/GHSA-1")).to.eql("GHSA-1");
   });
 });
