07779732e59f3a029587237aff8a8eb0d563e7bf
Quinn Turner <quinnturnertech@gmail.com>
2023-12-10 13:20:37 -0400

    Update Yarn 3

--- test/yarn-3-auditor.spec.ts
+++ test/yarn-3-auditor.spec.ts
@@ -2,7 +2,7 @@ import path from "path";
 import { SemVer } from "semver";
 import { performAuditTests } from "./yarn-auditor.js";
 
-const version = "3.3.1";
+const version = "3.7.0";
 
 const yarnAbsolutePath = path.resolve(__dirname, `./yarn-${version}.cjs`);
 
