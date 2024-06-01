e454a40f72ca5ce12302b7a737779663c9b97def
Quinn Turner <quinnturnertech@gmail.com>
2024-06-01 14:06:50 -0300

    Document Bun support and no Yarn v4 support

--- test/yarn-4-auditor.spec.ts
+++ test/yarn-4-auditor.spec.ts
@@ -1,14 +1,3 @@
-// import path from "path";
-// import { SemVer } from "semver";
-// import { performAuditTests } from "./yarn-auditor.js";
-
-// const version = "4.0.2";
-
-// const yarnAbsolutePath = path.resolve(__dirname, `./yarn-${version}.cjs`);
-
-// performAuditTests({
-//   yarnAbsolutePath,
-//   yarnVersion: new SemVer(version),
-// });
-
+// We don't currently support Yarn v4 because
+// it has most of the functionality that `audit-ci` provides built-in.
 export {};
